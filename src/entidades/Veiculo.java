69e31c86ca5ae765dcea2dd9ea1288e2a2278bdc
Pedro <pedrohenrique096k@gmail.com>
2023-02-04 17:20:06 -0300

    Refactor: Classe Veiculo
    Simplificando a maneira de calcular o aluguel de um veiculo e melhoria no construtor de classe

--- src/entidades/Veiculo.java
+++ src/entidades/Veiculo.java
@@ -1,7 +1,6 @@
 package entidades;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import interfaces.IVeiculo;
@@ -10,19 +9,51 @@ import interfaces.TipoVeiculo;
 public class Veiculo implements IVeiculo {
     private String placa;
     private Boolean disponivel;
-    private LocalDate diaAlugado;
-    private LocalDate diaDevolucao;
+    private LocalDateTime diaAlugado;
+    private LocalDateTime diaDevolucao;
     private TipoVeiculo tipoVeiculo;
 
+    public Veiculo(String placa, TipoVeiculo tipoVeiculo) {
+        this.placa = placa;
+        this.disponivel = true;
+        this.tipoVeiculo = tipoVeiculo;
+    }
 
-    @Override
-    public double calcularAluguel(LocalDateTime diaAlugado, LocalDateTime diaDevolucao) {
-        return calcularDias(diaAlugado, diaDevolucao) * 100;
+    public String getPlaca() {
+        return placa;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public LocalDateTime getDiaAlugado() {
+        return diaAlugado;
+    }
+
+    public void setDiaAlugado(LocalDateTime diaAlugado) {
+        this.diaAlugado = diaAlugado;
+    }
+
+    public LocalDateTime getDiaDevolucao() {
+        return diaDevolucao;
+    }
+
+    public void setDiaDevolucao(LocalDateTime diaDevolucao) {
+        this.diaDevolucao = diaDevolucao;
+    }
+
+    public TipoVeiculo getTipoVeiculo() {
+        return tipoVeiculo;
     }
 
     @Override
-    public long calcularDias(LocalDateTime diaAlugado, LocalDateTime diaDevolucao) {
-        return Duration.between(diaAlugado, diaDevolucao).toDays() + 1;
+    public double calcularAluguel() {
+        long dias = Duration.between(diaDevolucao, diaAlugado).toDays() + 1;
+        return dias * 100;
     }
-    
 }
--- src/interfaces/IVeiculo.java
+++ src/interfaces/IVeiculo.java
@@ -3,6 +3,5 @@ package interfaces;
 import java.time.LocalDateTime;
 
 public interface IVeiculo {
-    double calcularAluguel(LocalDateTime diaAlugado, LocalDateTime diaDevolucao);
-    long calcularDias(LocalDateTime diaAlugado, LocalDateTime diaDevolucao);
+    double calcularAluguel();
 }
