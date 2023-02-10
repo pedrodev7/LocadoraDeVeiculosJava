68ef029c77918b8cbe735cb481418c51946d65fa
Pedro <pedrohenrique096k@gmail.com>
2023-02-10 03:35:44 -0300

    Feat: Classe Recibo
    Classe responsavel por gerar recibos de aluguel e devolução de veiculos

---
+++ src/entidades/Recibo.java
@@ -0,0 +1,37 @@
+package entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Recibo {
+    public static void listarInformacoesDeAluguel(Veiculo veiculo, Pessoa pessoa, String local) {
+        System.out.println("=== VEICULO ALUGADO COM SUCESSO ===");
+        System.out.println("Carro: " + veiculo.getModelo());
+        System.out.println("Cliente: " + pessoa.getNome());
+        System.out.println("Data: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("Horario: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        System.out.println("Local: " + local);
+    }
+
+    public static void listarInformacoesDeDevolucao(Veiculo veiculo, Pessoa pessoa, String local, BigDecimal valor, long dias) {
+        System.out.println("=== VEICULO DEVOLVIDO COM SUCESSO ===");
+        System.out.println("Carro: " + veiculo.getModelo());
+        System.out.println("Cliente: " + pessoa.getNome());
+        System.out.println("Data: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("Horario: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        System.out.println("Local: " + local);
+        System.out.println("Quantidade de Diarias: " + dias);
+        if(dias > 5){
+            System.out.println("Desconto: SIM");
+            if(pessoa instanceof PessoaFisica){
+                System.out.println("Valor do Desconto: 5%");
+            }
+            if(pessoa instanceof PessoaJuridica){
+                System.out.println("Valor do Desconto: 10%");
+            }
+        }
+        System.out.println("Desconto: NÃO");
+        System.out.println("Valor a pagar em R$ " + valor.setScale(2));
+    }
+}
