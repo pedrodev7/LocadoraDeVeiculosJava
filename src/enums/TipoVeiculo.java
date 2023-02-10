c8528c4102f349705c88585ab6e21f20f982fc2f
Pedro <pedrohenrique096k@gmail.com>
2023-02-10 00:38:45 -0300

    Refactor: Classe Enum do TipoVeiculo

--- src/enums/TipoVeiculo.java
+++ src/enums/TipoVeiculo.java
@@ -1,5 +1,17 @@
 package enums;
 
 public enum TipoVeiculo {
-    CARRO, MOTO, CAMINHAO
+    HATCH_BACK("HATCH BACK"), 
+    SEDAN("SEDAN"), 
+    SUV("SUV");
+
+    private final String descricao;
+
+    private TipoVeiculo(String nomeTipo){
+        descricao = nomeTipo;
+    }
+
+    public String getNomeTipo(){
+        return descricao;
+    }
 }
